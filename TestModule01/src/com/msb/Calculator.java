a8a7313f256af2fa28efb341b2469fc45c97726a
dany <1343834092@qq.com>
2023-09-13 21:34:06 +0800

    提高了计算器的使用体验

--- TestModule01/src/com/msb/Calculator.java
+++ TestModule01/src/com/msb/Calculator.java
@@ -6,41 +6,50 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("欢迎使用简单计算器！");
-        System.out.print("请输入第一个数字: ");
-        double num1 = scanner.nextDouble();
+        while (true) {
+            System.out.print("请输入第一个数字: ");
+            double num1 = scanner.nextDouble();
 
-        System.out.print("请输入操作符 (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
+            System.out.print("请输入操作符 (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
 
-        System.out.print("请输入第二个数字: ");
-        double num2 = scanner.nextDouble();
+            System.out.print("请输入第二个数字: ");
+            double num2 = scanner.nextDouble();
 
-        double result;
+            double result;
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("错误：除数不能为零！");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("错误：无效的操作符！");
-                return;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("错误：除数不能为零！");
+                        continue; // 继续下一轮循环
+                    }
+                    break;
+                default:
+                    System.out.println("错误：无效的操作符！");
+                    continue; // 继续下一轮循环
+            }
+
+            System.out.println("结果: " + result);
+
+            System.out.print("是否继续计算？(y/n): ");
+            char choice = scanner.next().charAt(0);
+            if (choice != 'y' && choice != 'Y') {
+                break; // 退出循环
+            }
         }
 
-        System.out.println("结果: " + result);
+        System.out.println("感谢使用！");
     }
 }
