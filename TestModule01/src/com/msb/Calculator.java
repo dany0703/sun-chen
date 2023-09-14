6e467cb25346339604d4be864e1533426c5e459b
dany <1343834092@qq.com>
2023-09-14 10:42:07 +0800

    提高了计算器的使用体验

--- TestModule01/src/com/msb/Calculator.java
+++ TestModule01/src/com/msb/Calculator.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        int i;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
